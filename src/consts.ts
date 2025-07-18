6882d07162cdab9b8b335da9e23d9ed966f3f9a9
Grecia Valero <greciavalentinapv@hotmail.com>
2025-07-18 12:44:09 -0400

    Customize portfolio content

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Devolio';
-export const SITE_TAGLINE = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site meta description for SEO
+export const SITE_TITLE = 'lavendergarden🌱';
+export const SITE_TAGLINE = 'Mi Jardín Digital'; // site tagline
+export const SITE_DESCRIPTION = 'Personal digital garden where you will find information in English and Spanish of various topics that are of my interest. ESP: Jardín digital personal donde encontraras información en ingles y español de varios temas que son de mi interés.'; // site meta description for SEO
