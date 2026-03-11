4d01bdb45be3291c779195b40ceaaa620a961c9c
Grecia <greciavalentinapv@hotmail.com>
2026-03-11 19:06:31 -0400

    update

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,4 @@
 
 export const SITE_TITLE = 'Alma de estrella nocturna';
 export const SITE_TAGLINE = 'Jardín Digital🌱'; // site tagline
-export const SITE_DESCRIPTION = 'Personal digital garden where you will find content in English and Spanish of various topics that are of my interest. ESP: Jardín digital personal donde encontraras contenido en ingles y español de varios temas que son de mi interés.'; // site meta description for SEO
+export const SITE_DESCRIPTION = 'Jardín digital en español: videojuegos, arte, diseño, anime, música y espiritualidad. Comparto conocimiento y lo que me gusta.'; // site meta description for SEO
