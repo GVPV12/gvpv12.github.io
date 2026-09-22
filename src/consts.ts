726b5d8e3b18eccdafdc3e68bc5e6c6ba9ca254a
Grecia <greciavalentinapv@hotmail.com>
2026-09-21 21:49:23 -0400

    Update site branding and responsive menu

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Alma de estrella nocturna';
-export const SITE_TAGLINE = 'Jardín Digital🌱'; // site tagline
-export const SITE_DESCRIPTION = 'Jardín digital en español: videojuegos, arte, diseño, anime, música y espiritualidad. Comparto conocimiento y lo que me gusta.'; // site meta description for SEO
+export const SITE_TITLE = 'Crónicas de una menta divergente';
+export const SITE_TAGLINE = 'Tecnología, creatividad y cultura'; // site tagline
+export const SITE_DESCRIPTION = 'Jardín digital de Grecia V. sobre tecnología, diseño web, videojuegos, productividad, música, anime, cultura y espiritualidad.'; // site meta description for SEO
