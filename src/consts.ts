9713833918755d0b3c3a2235caebce7dfa672be8
Grecia <greciavalentinapv@hotmail.com>
2026-09-21 22:02:34 -0400

    update

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Crónicas de una menta divergente';
-export const SITE_TAGLINE = 'Tecnología, creatividad y cultura'; // site tagline
-export const SITE_DESCRIPTION = 'Jardín digital de Grecia V. sobre tecnología, diseño web, videojuegos, productividad, música, anime, cultura y espiritualidad.'; // site meta description for SEO
+export const SITE_TAGLINE = 'Jardín Digital🌱'; // site tagline
+export const SITE_DESCRIPTION = 'Jardín digital de Grecia V. sobre tecnología, diseño web, videojuegos, música, anime, cultura y espiritualidad.'; // site meta description for SEO
